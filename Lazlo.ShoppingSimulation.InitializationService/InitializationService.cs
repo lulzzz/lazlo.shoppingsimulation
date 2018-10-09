e38e37ea77bf8f2eff8e335e48f19ec17fea761f
Jack Bond <jackhanbond@hotmail.com>
2018-10-08 20:49:43 -0700

    More stub.

--- Lazlo.ShoppingSimulation.InitializationService/InitializationService.cs
+++ Lazlo.ShoppingSimulation.InitializationService/InitializationService.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Fabric;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Lazlo.ShoppingSimulation.Common.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
+using Newtonsoft.Json.Linq;
 
 namespace Lazlo.ShoppingSimulation.InitializationService
 {
@@ -16,15 +22,8 @@ namespace Lazlo.ShoppingSimulation.InitializationService
     {
         public InitializationService(StatelessServiceContext context)
             : base(context)
-        { }
-
-        /// <summary>
-        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
-        /// </summary>
-        /// <returns>A collection of listeners.</returns>
-        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
         {
-            return new ServiceInstanceListener[0];
+
         }
 
         /// <summary>
@@ -33,18 +32,34 @@ namespace Lazlo.ShoppingSimulation.InitializationService
         /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
         protected override async Task RunAsync(CancellationToken cancellationToken)
         {
-            // TODO: Replace the following sample code with your own logic 
-            //       or remove this RunAsync override if it's not needed in your service.
-
-            long iterations = 0;
-
-            while (true)
+            try
             {
-                cancellationToken.ThrowIfCancellationRequested();
+                string code;
 
-                ServiceEventSource.Current.ServiceMessage(this.Context, "Working-{0}", ++iterations);
+                using (HttpClient client = new HttpClient())
+                {
+                    code = await client.GetStringAsync("https://devlng.blob.core.windows.net/simulation/PosDeviceLicenseCodes.json?st=2017-10-09T02%3A55%3A00Z&se=2028-10-10T02%3A55%3A00Z&sp=rl&sv=2018-03-28&sr=b&sig=uFCkCVp2zN2trKUJY2nxLozNfzB9%2B%2BlNTHKqkFSCfg0%3D");
+                }
 
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+                JArray codes = JArray.Parse(code);
+
+                foreach (JObject jo in codes)
+                {
+                    Guid licenseCode = (Guid)jo["id"];
+
+                    //ActorId partitionActorId = new ActorId(Guid.Parse("0515122e-337e-47da-aa84-2b1f5b6e5d23"));
+
+                    //IPosDeviceSimulationActor posDeviceActor = ActorProxy.Create<IPosDeviceSimulationActor>(partitionActorId, new Uri("fabric:/LotteryNG.ServiceFabric.Simulation/PlayerActorService"));
+
+                    //await posDeviceActor.InitializeAsync();
+                }
+
+
+            }
+
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
             }
         }
     }
